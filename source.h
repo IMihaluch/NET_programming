efaad92c46eb7917c6e9cd25d97dc676376a7239
IMihaluch <84381638+IMihaluch@users.noreply.github.com>
2022-11-20 15:41:12 +0300

    Create source.h

---
+++ source.h
@@ -0,0 +1,36 @@
+
+#ifndef LIBHTTPSERVER_FILESTORAGE_H
+#define LIBHTTPSERVER_FILESTORAGE_H
+
+#include "httpserver.hpp"
+#include <thread>
+#include <iostream>
+
+const std::string AUTH_FILE{ "auth.txt" };
+
+struct Login{
+    std::string user;
+    std::string password;
+
+    const bool operator==(const Login &other) const
+    {
+        return this->user == other.user && this->password == other.password;
+    }
+};
+
+
+class source : public httpserver::http_resource{
+private:
+    std::vector<Login> logins_;
+
+    void inflateLoginBase();
+    const bool authenticate(const Login&) const;
+
+public:
+    source();
+
+    const std::shared_ptr<httpserver::http_response>
+        render(const httpserver::http_request&);
+};
+
+#endif //LIBHTTPSERVER_FILESTORAGE_H
